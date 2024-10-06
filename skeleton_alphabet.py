b9f7713c1c65b57586e52af96da87d8233a715d7
Michael Platzer <michael.platzer@axelera.ai>
2024-10-06 13:48:00 +0000

    Add digits and period to characters

--- skeleton_alphabet.py
+++ skeleton_alphabet.py
@@ -41,7 +41,7 @@ upper_circ = {key: dim for char, val in circ_dia.items() for key, dim in {
 
 dimensions = base_dim | circ_dia | upper_circ | A_dim
 
-svg_size       = (27, 4)
+svg_size       = (36, 6)
 svg_attributes = {
   'style': "fill:none;stroke:black;stroke-width:0.1;stroke-linecap:round;stroke-linejoin:round"
 }
@@ -60,6 +60,11 @@ with open('skeleton_alphabet.svg', 'w') as svg:
   xpos = .5
   for char in 'abcdefghijklmnopqrstuvwxyz':
     svg.write(f"  <path transform=\"translate({xpos},3.5)\" d=\"{skel_alpha[char]['d'].format(**dimensions)}\" />\n")
+    xpos += eval(skel_alpha[char]['w'].format(**dimensions)) + .25
+
+  xpos = .5
+  for char in '0123456789':
+    svg.write(f"  <path transform=\"translate({xpos},5.5)\" d=\"{skel_alpha[char]['d'].format(**dimensions)}\" />\n")
     xpos += eval(skel_alpha[char]['w'].format(**dimensions)) + .25
 
   svg.write("</svg>\n")
